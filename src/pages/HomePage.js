8de6364910b1cc7415ecae089521451a6d95fbb2
autostralis <autostralisofficial@gmail.com>
2025-08-25 17:35:08 +0530

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Spanzor</h1>
+      <p>Connecting brands and influencers for powerful collaborations.</p>
+      <a href="/login">Sign In</a>
+    </div>
+  );
+}
